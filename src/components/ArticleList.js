b0724e52a524ecddc44314d5c3649f758d2849fa
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-21 09:35:41 +0300

    Completed Assignment

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,7 +6,7 @@ import blogData from '../data/blog'
 function ArticleList(){
     const articleList = blogData.posts.map(article => {
         console.log("article: ", article)
-        return <Article title={article.id} preview={article.preview} date={article.date}/>
+        return <Article key={article.id} title={article.title} preview={article.preview} date={article.date}/>
     })
 
     console.log("article list: ", articleList)
